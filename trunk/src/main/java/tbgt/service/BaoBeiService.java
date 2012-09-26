8ba8ca90618727bb2196b5f84e6671bca9cd349f
licweiwei@gmail.com <licweiwei@gmail.com@b3ac3b00-a4e9-f988-c102-0964ac1fe5c1>
2012-09-26 09:33:44 +0000

    update

--- trunk/src/main/java/tbgt/service/BaoBeiService.java
+++ trunk/src/main/java/tbgt/service/BaoBeiService.java
@@ -1,12 +1,15 @@
 package tbgt.service;
 
 import tbgt.domain.Baobei;
+import tbgt.web.paging.PaginationTO;
 
 import java.util.List;
 
 public interface BaoBeiService {
 
     public List<Baobei> getAllBaobei();
+
+    public PaginationTO getBaobeiWithPaging();
 
     public void deleteBaobei(int id);
 
---
+++ trunk/src/main/java/tbgt/web/paging/PagingFilter.java
@@ -0,0 +1,41 @@
+package tbgt.web.paging;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Map;
+
+public class PagingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String iDisplayStartStr = httpRequest.getParameter("iDisplayStart");
+        if (iDisplayStartStr != null) {
+            int iDisplayStart = Integer.valueOf(iDisplayStartStr);
+            int iDisplayLength = Integer.valueOf(httpRequest.getParameter("iDisplayLength"));
+            int sEcho = Integer.valueOf(httpRequest.getParameter("sEcho"));
+            int iSortCol_0 = Integer.valueOf(httpRequest.getParameter("iSortCol_0"));
+            PagingContextHolder.clear();
+            PaginationTO paginationTO = new PaginationTO();
+            paginationTO.setiDisplayLength(iDisplayLength);
+            paginationTO.setiDisplayStart(iDisplayStart);
+            paginationTO.setsEcho(sEcho);
+            paginationTO.setsSearch(httpRequest.getParameter("sSearch"));
+            paginationTO.setiSortCol_0(iSortCol_0);
+            paginationTO.setsSortDir_0(httpRequest.getParameter("sSortDir_0"));
+            paginationTO.setsSortColumn_0(httpRequest.getParameter("mDataProp_"+iSortCol_0));
+            PagingContextHolder.set(paginationTO);
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
